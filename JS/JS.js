e3c2bd4ce05fa8b2bad9035411f9b3f9e6b8938b
SalmaQassem <salma99kasem@gmail.com>
2023-02-09 13:47:00 +0200

    Added scroll features

--- JS/JS.js
+++ JS/JS.js
@@ -1,23 +1,30 @@
+/*Start Event Listeners*/
+window.addEventListener("beforeunload", function (){
+    window.scrollTo(0, 0);
+});
+
 /*Start play video button*/
-document.getElementById("playVideo").onclick = function () {
+document.getElementById("playVideo").addEventListener("click", function () {
     document.getElementById("video").play();
     document.getElementById("playVideo").style.visibility = "hidden";
-};
-document.getElementById("video").onclick = function () {
+});
+document.getElementById("video").addEventListener("click", function () {
     if(document.getElementById("video").played) {
         document.getElementById("video").pause();
         document.getElementById("playVideo").style.visibility = "initial";
     }
-};
+});
 
-document.getElementById("events-playVideo").onclick = function () {
+document.getElementById("events-playVideo").addEventListener("click", function () {
     document.getElementById("events-video").play();
     document.getElementById("events-playVideo").style.visibility = "hidden";
-};
-document.getElementById("events-video").onclick = function () {
+});
+document.getElementById("events-video").addEventListener("click",function () {
     if(document.getElementById("events-video").played) {
         document.getElementById("events-video").pause();
         document.getElementById("events-playVideo").style.visibility = "initial";
     }
-};
+});
 /*End play video button*/
+
+/*End Event Listeners*/
