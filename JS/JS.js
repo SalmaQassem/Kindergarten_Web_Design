56eec354ee8e4f33327377e26c40564a74453a52
SalmaQassem <salma99kasem@gmail.com>
2023-02-11 14:42:01 +0200

    Added scroll to the top event on page refresh & scroll to the top button

--- JS/JS.js
+++ JS/JS.js
@@ -1,36 +1,65 @@
 /*Start Event Listeners*/
 
 window.addEventListener("beforeunload", function () {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior: "smooth",
+    });
 });
 
-/*Start play video button*/
-document.getElementById("playVideo").addEventListener("click", function () {
-    document.getElementById("video").play();
-    document.getElementById("playVideo").style.visibility = "hidden";
-});
-document.getElementById("video").addEventListener("click", function () {
-    if (document.getElementById("video").played) {
-        document.getElementById("video").pause();
-        document.getElementById("playVideo").style.visibility = "initial";
-    }
-});
+/*Start Play Video Event Listener*/
 
-document.getElementById("events-playVideo").addEventListener("click", function () {
-    document.getElementById("events-video").play();
-    document.getElementById("events-playVideo").style.visibility = "hidden";
-});
-document.getElementById("events-video").addEventListener("click", function () {
-    if (document.getElementById("events-video").played) {
-        document.getElementById("events-video").pause();
-        document.getElementById("events-playVideo").style.visibility = "initial";
-    }
-});
-/*End play video button*/
+/*Global Variables*/
+let playBtn = document.querySelectorAll(".playVideo");
+let videos = document.querySelectorAll("video");
+let upButton = document.querySelector(".up");
 
+for (let i = 0; i < playBtn.length; i++) {
+    playBtn[i].addEventListener("click", function () {
+        videos[i].play();
+        playBtn[i].style.visibility = "hidden";
+    });
+}
+for (let i = 0; i < videos.length; i++) {
+    videos[i].addEventListener("click", function () {
+        if (videos[i].played) {
+            videos[i].pause();
+            playBtn[i].style.visibility = "initial";
+        }
+    });
+}
+
+/*End Play Video Event Listener*/
+
+/*Start Nav-link Event Listener*/
 document.addEventListener("click", function (e) {
     if (e.target.classList.contains("nav-link")) {
         document.querySelector("#offCanvasNavbar").classList.remove("show");
     }
 });
+
+/*End Nav-link Event Listener*/
+
+/*Start Up Button Event Listener*/
+
+//Display Up Button
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 400) {
+        upButton.style.display = "block";
+    } else
+        upButton.style.display = "none";
+});
+
+//Add Up Button Click Event
+upButton.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
+
+/*End Up Button Event Listener*/
+
 /*End Event Listeners*/
