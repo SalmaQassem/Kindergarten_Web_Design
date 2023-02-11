61956909be8e80df67b71663d3b2589df0a006dd
SalmaQassem <salma99kasem@gmail.com>
2023-02-11 12:42:25 +0200

    Added a feature to collapse the navbar when a link is clicked

--- JS/JS.js
+++ JS/JS.js
@@ -1,5 +1,6 @@
 /*Start Event Listeners*/
-window.addEventListener("beforeunload", function (){
+
+window.addEventListener("beforeunload", function () {
     window.scrollTo(0, 0);
 });
 
@@ -9,7 +10,7 @@ document.getElementById("playVideo").addEventListener("click", function () {
     document.getElementById("playVideo").style.visibility = "hidden";
 });
 document.getElementById("video").addEventListener("click", function () {
-    if(document.getElementById("video").played) {
+    if (document.getElementById("video").played) {
         document.getElementById("video").pause();
         document.getElementById("playVideo").style.visibility = "initial";
     }
@@ -19,12 +20,17 @@ document.getElementById("events-playVideo").addEventListener("click", function (
     document.getElementById("events-video").play();
     document.getElementById("events-playVideo").style.visibility = "hidden";
 });
-document.getElementById("events-video").addEventListener("click",function () {
-    if(document.getElementById("events-video").played) {
+document.getElementById("events-video").addEventListener("click", function () {
+    if (document.getElementById("events-video").played) {
         document.getElementById("events-video").pause();
         document.getElementById("events-playVideo").style.visibility = "initial";
     }
 });
 /*End play video button*/
 
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("nav-link")) {
+        document.querySelector("#offCanvasNavbar").classList.remove("show");
+    }
+});
 /*End Event Listeners*/
